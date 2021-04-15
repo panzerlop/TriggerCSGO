edbb07e18707cf9da5860d15b53eec75af7234ca
vladislavkovalskyi <v.darknesssb@gmail.com>
2021-04-15 12:18:15 +0300

    v1.1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import keyboard
+import mouse
 import pymem
 
 from offsets import dwLocalPlayer, m_iCrosshairId, dwEntityList, m_iTeamNum, dwForceAttack
@@ -8,24 +9,27 @@ client = pymem.process.module_from_name(process.process_handle, "client.dll").lp
 
 
 def main():
+    print("v1.1 Started!\n"
+          "\"ALT\" -- Activate")
     shooting = False
     while True:
-        player_object = process.read_int(client + dwLocalPlayer)
-        if keyboard.is_pressed("shift"):
-            entity = process.read_int(player_object + m_iCrosshairId)
+        try:
+            player_object = process.read_int(client + dwLocalPlayer)
+            if keyboard.is_pressed("alt"):
+                entity = process.read_int(player_object + m_iCrosshairId)
 
-            if 0 < entity <= 64:
-                entity = process.read_int(client + dwEntityList + (entity - 1) * 0x10)
-                entity_team = process.read_int(entity + m_iTeamNum)
-                player_team = process.read_int(player_object + m_iTeamNum)
+                if 0 < entity <= 64:
+                    entity = process.read_int(client + dwEntityList + (entity - 1) * 0x10)
+                    entity_team = process.read_int(entity + m_iTeamNum)
+                    player_team = process.read_int(player_object + m_iTeamNum)
 
-                if player_team != entity_team:
-                    shooting = True
-                    process.write_int(client + dwForceAttack, 5)
+                    if player_team != entity_team:
+                        mouse.click()
 
-        if not keyboard.is_pressed("space") and shooting is True:
-            process.write_int(client + dwForceAttack, 4)
-            shooting = False
+            if not keyboard.is_pressed("alt") and shooting is True:
+                shooting = False
+        except Exception as error:
+            print(f"Error: {error}")
 
 
 main()
